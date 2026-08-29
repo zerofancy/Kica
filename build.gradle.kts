1d26395cc69fc4540776f28292c43c0559325d87
zerofancy <ntutn.top@gmail.com>
2026-08-29 17:15:52 +0800

    chore: 升级 Android Gradle Plugin 至 9.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     kotlin("plugin.serialization") version "2.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
     id("org.jetbrains.compose") version "1.11.1" apply false
-    id("com.android.application") version "9.3.1" apply false
-    id("com.android.kotlin.multiplatform.library") version "9.3.1" apply false
+    id("com.android.application") version "9.3.2" apply false
+    id("com.android.kotlin.multiplatform.library") version "9.3.2" apply false
     id("app.cash.sqldelight") version "2.1.0" apply false
 }
 
